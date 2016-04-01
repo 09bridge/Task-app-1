a3995f5577202c50bf05491b69d2e9d52dd3aee6
09bridge <bridge.n.me@gmail.com>
2016-04-01 07:17:41 +0300

    made correction and refactoring code

--- app/src/main/java/internship/yalantis/task_app_1/MainActivity.java
+++ app/src/main/java/internship/yalantis/task_app_1/MainActivity.java
@@ -1,123 +1,58 @@
 package internship.yalantis.task_app_1;
 
-import android.content.Context;
-import android.net.Uri;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.DisplayMetrics;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-
-import com.squareup.picasso.Picasso;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    String [] dataSet;
-    ImageButton backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        dataSet = new String[]{
-                "http://i.dailymail.co.uk/i/pix/2014/04/11/article-2602528-1D06B2D200000578-424_634x471.jpg",
-                "http://memesvault.com/wp-content/uploads/Sad-Cat-Meme-04.jpg",
-                "https://s-media-cache-ak0.pinimg.com/736x/bf/fd/0e/bffd0e24f35c7e61f0b8bc022b94a213.jpg",
-                "http://raisinghappykittens.com/wp-content/uploads/2015/04/sad_cat.jpg",
-                "http://www.she-kicks-she-throws.com/wp-content/uploads/sad-cat-fi-720x340.jpg",
-                "http://vignette1.wikia.nocookie.net/steven-universe/images/8/8a/Sad-cat.jpg/revision/latest?cb=20150827161123"
-        };
+        //Find a screen width for image resize
+        DisplayMetrics displayMetrics = new DisplayMetrics ();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int dpWidth  = displayMetrics.widthPixels;
+        int correction = Math.round(getResources().getDimension(R.dimen.padding_horizontal));
+        int imageNewSize = dpWidth/2 - (3 * correction / 2);
 
-        backButton = (ImageButton) findViewById(R.id.backButton);
+        Resources mLinks = getResources();
+        String[] mDataSet = mLinks.getStringArray(R.array.links);
 
         RecyclerView imageRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
         imageRecyclerView.setHasFixedSize(true);
 
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this, 0, false);
-        imageRecyclerView.setLayoutManager(mLayoutManager);
+        RecyclerView.LayoutManager imageLayoutManager = new LinearLayoutManager(this, 0, false);
+        imageRecyclerView.setLayoutManager(imageLayoutManager);
 
-        RecyclerView.Adapter mAdapter = new MyAdapter(dataSet);
-        imageRecyclerView.setAdapter(mAdapter);
+        RecyclerView.Adapter imageAdapter = new ImageAdapter(mDataSet, imageNewSize);
+        imageRecyclerView.setAdapter(imageAdapter);
 
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-
-    }
-
-    public void closeApp(View v) {
-        this.finish();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        /*if (id == R.id.action_settings) {
-            return true;
-        }*/
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ImageViewHolder> {
-        private String [] mDataSet;
-        public class ImageViewHolder extends RecyclerView.ViewHolder {
-            ImageView imageViewIcon;
-            public ImageViewHolder(View itemView) {
-                super(itemView);
-                imageViewIcon = (ImageView) itemView.findViewById(R.id.recycler_image_item);
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(mToolbar);
+        mToolbar.setTitleTextColor(Color.WHITE);
+        mToolbar.setNavigationIcon(R.drawable.ic_arrow_back);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                MainActivity.this.finish();
             }
-        }
-        public MyAdapter(String [] mDataSet) {
-            this.mDataSet = mDataSet;
-        }
+        });
 
-        @Override
-        public ImageViewHolder onCreateViewHolder(ViewGroup parent,
-                                                       int viewType) {
-            View v = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.recycler_image_item, parent, false);
-            // set the view's size, margins, paddings and layout parameters
+    }
 
-            ImageViewHolder vh = new ImageViewHolder(v);
-            return vh;
-        }
-
-        @Override
-        public void onBindViewHolder(ImageViewHolder holder, int position) {
-
-            Uri imageUri = Uri.parse(mDataSet[position]);
-            Context context = holder.imageViewIcon.getContext();
-
-            Picasso.with(context).load(imageUri).into(holder.imageViewIcon);
-
-
-
-        }
-
-        @Override
-        public int getItemCount() {
-            return mDataSet.length;
-        }
-
+    public void onClickView(View view) {
+        String viewName = view.getClass().getSimpleName();
+        Toast infoToast = Toast.makeText(getApplicationContext(), viewName, Toast.LENGTH_SHORT);
+        infoToast.show();
     }
 }
